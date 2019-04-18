90e9855d10e0def75c91587466f77e46448e9cdc
Tomasz Jasiukiewicz <t.jasiukiewicz@google.com>
2019-04-18 15:32:01 +0200

    full

--- func_decorator/main.cpp
+++ func_decorator/main.cpp
@@ -12,9 +12,27 @@ double addShipping(double val) {
     return val + 0.01;
 }
 
+namespace detail {
+
+template<typename ArgType, typename Function>
+auto applyFuncs_impl(ArgType arg, Function func) {
+    return func(arg);
+}
+template<typename ArgType, typename Func, typename... Functions>
+auto applyFuncs_impl(ArgType arg, Func func, Functions... funcs) {
+    return detail::applyFuncs_impl(detail::applyFuncs_impl(arg, func), funcs...);
+}
+
+
+} // namespace detail
+
+template<typename ArgType, typename ... Funcs>
+auto applyFuncs(ArgType value, Funcs... funcs) {
+    return detail::applyFuncs_impl(value, funcs...);
+}
 
 int main() {
     std::cout << addShipping(addFee(addTax(100.0))) << '\n';
     //std::cout << decorate<double>(100.0).decor(addTax).decor(addFee).decor(addShipping) << '\n';
-    std::cout << applyFuncs(100.0, addTax, addFee, addTax, [](double val) { return 10000.0; }) << '\n';
+    std::cout << applyFuncs(100.0, addTax, addFee, addTax, [](double val) { return val * 2.0; }) << '\n';
 }
